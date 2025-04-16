fe75cf09f3b631160cdbe2acbf4a1977366525cd
MarkNewcomb1 <wlmmn@hotmail.com>
2025-04-16 10:34:58 -0600

    initial styling and title and subtitle

---
+++ app/index.jsx
@@ -0,0 +1,44 @@
+import { Text, View, StyleSheet, Image } from "react-native";
+import PostItImage from '@/assets/images/post-it.png';
+
+const HomeScreen = () => {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Image source={PostItImage} style={styles.image} />
+      <Text style={styles.title}>Welcome To Notes App</Text>
+      <Text style={styles.subtitle}>Capture your thoughts anytime, anywhere</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#f8f9fa',
+  },
+  image: {
+    width: 100, // is 100dp, not 100 physical pixels
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+})
+
+export default HomeScreen;
